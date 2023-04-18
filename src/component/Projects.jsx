ddb15b76348ed90d6405457830d1ee609e143737
Soyeb <98175646+Soyeb01@users.noreply.github.com>
2023-04-18 19:39:52 +0530

    Update Projects.jsx

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
       <div id="projects" className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
           <h1 className='text-4xl font-bold text-center text-gray-900 dark:text-gray-100 w-fit mx-auto px-4 pb-4 border-b-2 border-b-gray-800 dark:border-b-gray-200'>Projects</h1>
           <p className='text-center dark:text-gray-200 py-8'>
-              These are my lateset projects I've been working on. I really enjoy making projects to polish and learn new things.
+              These are my latest projects I've been working on. I really enjoy making projects to polish and learn new things.
           </p>
           <div className='grid sm:grid-cols-2 gap-12'>
               <ProjectItem img={QuoteGen} title="QuoteGen App" link="https://github.com/Soyeb01/quoteGenerator" tech="HTML | SCSS | JavaScript | API"/>
