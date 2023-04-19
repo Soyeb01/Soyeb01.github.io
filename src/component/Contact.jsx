5dabcd6356392f99424cc40bf8be5ecb324d853b
Soyeb <98175646+Soyeb01@users.noreply.github.com>
2023-04-19 14:30:16 +0530

    Update Contact.jsx

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -11,24 +11,24 @@ const Contact = () => {
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                     <div className='flex flex-col'>
                         <label className='uppercase font-semibold dark:text-gray-200 text-sm py-2'>Name</label>
-                        <input className='border-2 rounded-lg p-3 border-gray-300' type="text" name="name"/>
+                        <input required className='border-2 rounded-lg p-3 border-gray-300' type="text" name="name"/>
                     </div>
                     <div className='flex flex-col'>
                         <label className='uppercase font-semibold dark:text-gray-200 text-sm py-2'>Phone Number</label>
-                        <input className='border-2 rounded-lg p-3 border-gray-300' maxLength="10" type="text" name="number" />
+                        <input required className='border-2 rounded-lg p-3 border-gray-300' maxLength="10" type="text" name="number" />
                     </div>
                 </div>
                 <div className='flex flex-col py-2'>
                     <label className='uppercase font-semibold dark:text-gray-200 text-sm py-2'>Email</label>
-                    <input className='border-2 rounded-lg p-3 border-gray-300' type="email" name="email"/>
+                    <input required className='border-2 rounded-lg p-3 border-gray-300' type="email" name="email"/>
                 </div>
                 <div className='flex flex-col py-2'>
                     <label className='uppercase font-semibold dark:text-gray-200 text-sm py-2'>Subject</label>
-                    <input className='border-2 rounded-lg p-3 border-gray-300' type="text" name="subject"/>
+                    <input required className='border-2 rounded-lg p-3 border-gray-300' type="text" name="subject"/>
                 </div>
                 <div className='flex flex-col py-2'>
                     <label className='uppercase font-semibold dark:text-gray-200 text-sm py-2' >Message</label>
-                    <textarea className='border-2 rounded-lg p-3 border-gray-300' name="message" cols="30" rows="10"></textarea>
+                    <textarea required className='border-2 rounded-lg p-3 border-gray-300' name="message" cols="30" rows="10"></textarea>
                 </div>
                 
                 <button type='submit' className='bg-gray-900 font-semibold dark:bg-gray-200 text-gray-100 dark:text-gray-800 mt-4 w-full p-4 rounded-lg hover:bg-gray-100 hover:text-gray-900 hover:border-2 hover:border-gray-700'>Send Message</button>
